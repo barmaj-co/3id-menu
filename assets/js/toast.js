2c8aac4d84c3de6a52e0a0051658d249650e370c
barmaj-co <59221358+barmaj-co@users.noreply.github.com>
2024-06-10 12:03:26 +0300

    Add showSuccessToast in toast.js

--- assets/js/toast.js
+++ assets/js/toast.js
@@ -10,3 +10,17 @@ function showErrorToast(message) {
 
     setTimeout(() => $('#toastContainer').empty(), 2000);
 }
+
+
+function showSuccessToast(message) {
+    var toastHTML = `
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            ${message}
+        </div>
+    `;
+    
+    $('#toastContainer').append(toastHTML);
+    $('.toast').toast('show');
+
+    setTimeout(() => $('#toastContainer').empty(), 2000);
+}
